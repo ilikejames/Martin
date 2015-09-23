c53dae900842fbf91da0a149071e9599fd57c516
James Mountain <james.mountain@gmail.com>
2015-09-23 13:22:35 +0100

    Fixed broken test due to aync $httpBackend

--- martinAppWeb/public/app/common/services/TextService.test.js
+++ martinAppWeb/public/app/common/services/TextService.test.js
@@ -6,37 +6,32 @@
 
 		var textService,
 			$httpBackend,
-			getTextRequest;
+			$rootScope;
 
 
 		beforeEach(module('app.common.services'));
 
-		beforeEach(inject(function($injector, _TextService_){
 
+		beforeEach(inject(function($injector, _$rootScope_, _TextService_){
 			textService = _TextService_;
 			$httpBackend = $injector.get('$httpBackend');
-
-			getTextRequest = $httpBackend.when('GET', '/api/statement/james')
-                            .respond({ obj : 1 });
-
+			$rootScope = _$rootScope_;
 		}));
 
 
-		afterEach(function(done) {
+		afterEach(function() {
 			$httpBackend.verifyNoOutstandingExpectation();
 			$httpBackend.verifyNoOutstandingRequest();
-			done();
 		});
 
 
-		it('Should make a call to the api', function(done) {
+		it('Should make a call to the api', function() {
 
-			$httpBackend.expectGET('/api/statement/james');
+			$httpBackend.expectGET('/api/statement/james').respond({ obj : 1 });
 
 			textService.getText('james')
 			.then(function(response) {
 				expect(response.data).toEqual({ obj : 1});
-				done();
 			});
 
 			$httpBackend.flush();
